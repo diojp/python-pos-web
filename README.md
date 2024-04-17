a0e13710664873a23d783a6c652425cd6d3f302b
Diego Magno <dio_jp@hotmail.com>
2024-04-17 16:37:06 -0300

    Alterações Importantes no pedidos e readme
    Inserção da parte de pedidos e a atualização do readme

--- README.md
+++ README.md
@@ -1,2 +1,171 @@
 # python-pos-web
- Projeto Pyhtoh MongDB e Postgres
+#### Projeto Pyhton em MongDB e Postgres 
+Disciplina Modelagem de Banco de Dados (SQL e NoSQL)
+Curso de PÓS GRADUAÇÃO em Desenvolvimento Web Full Stack - UNIPÊ.
+
+## Funcionalidades
+
+- Projeto em Python
+- Integração com Banco de Dados Postgres & MongoDB
+
+
+
+## Instalação
+
+Instale a biblioteca psycopg2 com o comando pip
+Se faz necessário a instalação dessa biblioteca pois é um adaptador de Python para o Postgres!!!
+
+```bash
+  pip install psycopg2
+```
+
+Configure corretamente o SQLALCHEMY_DATABASE_URI para funcionar
+```
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/aula_banco'
+```
+
+- Nome do Usuário do Banco = postgres;
+- Password do Banco = postgres;
+- Servidor = localhost;
+- Porta Padrão = 5432;
+- Nome do BD = aula_banco.
+
+    
+
+## Scripts do Banco de Dados
+
+#### Criação das Sequences para Cliente e Produto
+
+```sql
+
+  CREATE SEQUENCE seq_id_cliente
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+CREATE SEQUENCE seq_id_produto
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+```
+
+#### Criação das Tabelas para cliente e produto
+
+```sql
+
+  CREATE TABLE clientes (
+	id int4 DEFAULT nextval('seq_id_cliente'::regclass) NOT NULL,
+	nome varchar(255) NULL,
+	email varchar(255) NULL,
+	cpf varchar(11) NULL,
+	data_nascimento date NULL,
+	CONSTRAINT clientes_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE produtos (
+	id_produto int4 DEFAULT nextval('seq_id_produto'::regclass) NOT NULL,
+	nome varchar(255) NULL,
+	descricao text NULL,
+	preco numeric NULL,
+	categoria varchar(255) NULL,
+	CONSTRAINT produtos_pkey PRIMARY KEY (id_produto)
+);
+```
+
+#### Populando as Tabelas para Clientes e Produtos
+
+```sql
+INSERT INTO clientes (nome,email,cpf,data_nascimento) VALUES
+('Robson Junior','joaosilva@email.com','72349257899','2024-04-17'),
+('Diego Magno','diego@email.com','43020254094','2024-04-18');
+
+INSERT INTO produtos (nome,descricao,preco,categoria) VALUES
+('Feijão','pacote de feijão',10.0,'NÃO PERECÍCEL'),
+('SAMSUNG 60','SAMSUNG',10.0,'TV ELETRÔNICOS');
+```
+
+## Documentação da API
+Acessando a API na porta padrão 5000
+
+### CRUD Clientes
+
+#### Retorna todos os Clientes
+
+```http
+  GET http://localhost:5000/clientes/  
+```
+#### Insere um Cliente
+
+```http
+  POST http://localhost:5000/cliente/
+```
+
+#### Altera / Atualiza um Cliente
+
+```http
+  PUT http://localhost:5000/cliente/${id}
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `id`      | `int` | **Obrigatório**. O ID do cliente que você quer atualizar |
+
+#### Deleta / Remove um Cliente
+
+```http
+  PUT http://localhost:5000/cliente/${id}
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `id`      | `int` | **Obrigatório**. O ID do cliente que você quer excluir |
+
+
+### CRUD Produtos
+
+#### Retorna todos os Produtos
+
+```http
+  GET http://localhost:5000/produtos/  
+```
+#### Insere um Produto
+
+```http
+  POST http://localhost:5000/produto/
+```
+
+#### Altera / Atualiza um Produto
+
+```http
+  PUT http://localhost:5000/produto/${id}
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `id`      | `int` | **Obrigatório**. O ID do produto que você quer atualizar |
+
+#### Deleta / Remove um Produto
+
+```http
+  PUT http://localhost:5000/produto/${id}
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `id`      | `int` | **Obrigatório**. O ID do produto que você quer excluir |
+
+
+
+
+
+## Autores
+
+- [@Diego Magno Tavares da Silva](https://www.github.com/diojp)
+- [@Robson Vieira Cavalcante Júnior](https://www.github.com/diojp)
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from config import SQLALCHEMY_DATABASE_URI, mongodb
+from bson import ObjectId
+
+from config import SQLALCHEMY_DATABASE_URI, mongodb, pedidos_collection
 from flask import Flask, jsonify, request
 from sqlalchemy import Integer, String, Float, Date
 from flask_sqlalchemy import SQLAlchemy
@@ -34,7 +36,22 @@ class Clientes(postgres.Model):
             "nome": self.nome,
             "email": self.email,
             "cpf": self.cpf,
-            "dataNascimento": self.data_nascimento
+            "data_nascimento": self.data_nascimento
+        }
+
+class Pedidos():
+    def __init__(self, id_cliente, id_produto, data_pedido, valor_pedido):
+        self.id_cliente = id_cliente
+        self.id_produto = id_produto
+        self.data_pedido = data_pedido
+        self.valor_pedido = valor_pedido
+
+    def serialize(self):
+        return {
+            "id_cliente": self.id_cliente,
+            "id_produto": self.id_produto,
+            "data_pedido": self.data_pedido,
+            "valor_pedido": self.valor_pedido
         }
 
 @app.route("/")
@@ -64,24 +81,166 @@ def set_produto():
         print(f"Erro: {e}")
         return jsonify(), 400
 
-@app.route("/clientes", methods=['POST'])
-def set_clientes():
-    dados = request.get_json()
-    cliente = Clientes(
-        nome=dados["nome"],
-        email=dados["email"],
-        cpf=dados["cpf"],
-        data_nascimento=dados["data_nascimento"]
-    )
-    postgres.session.add(cliente)
-    postgres.session.commit()
-    return jsonify(cliente.serialize()), 201
+@app.route("/produto/<int:id>", methods=['PUT'])
+def update_produto(id):
+    try:
+        dados = request.get_json()
+
+        produto = postgres.session.query(Produtos).get(id)
+        produto.nome = dados["nome"]
+        produto.descricao = dados["descricao"]
+        produto.preco = dados["preco"]
+        produto.nome = dados["categoria"]
+
+        postgres.session.commit()
+        return jsonify(produto.serialize()), 201
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro ao alterar os dados", 400
+
+
+@app.route("/produto/<int:id>", methods=['DELETE'])
+def delete_produto(id):
+    try:
+        produto = postgres.session.query(Produtos).get(id)
+
+        postgres.session.delete(produto)
+        postgres.session.commit()
+        return "Produto Excluido com Sucesso", 201
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro ao excluir o produto", 400
 
 @app.route("/clientes", methods=['GET'])
 def get_clientes():
     clientes = Clientes.query.all()
 
     return jsonify([cliente.serialize() for cliente in clientes])
+
+@app.route("/cliente", methods=['POST'])
+def set_cliente():
+    try:
+        dados = request.get_json()
+        cliente = Clientes(
+            nome=dados["nome"],
+            email=dados["email"],
+            cpf=dados["cpf"],
+            data_nascimento=dados["data_nascimento"]
+        )
+        postgres.session.add(cliente)
+        postgres.session.commit()
+        return jsonify(cliente.serialize()), 201
+    except Exception as e:
+        print(f"Erro: {e}")
+        return jsonify(), 400
+
+@app.route("/cliente/<int:id>", methods=['PUT'])
+def update_cliente(id):
+    try:
+        dados = request.get_json()
+
+        cliente = postgres.session.query(Clientes).get(id)
+        cliente.nome = dados["nome"]
+        cliente.email = dados["email"]
+        cliente.cpf = dados["cpf"]
+        cliente.data_nascimento = dados["data_nascimento"]
+
+        postgres.session.commit()
+        return jsonify(cliente.serialize()), 201
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro ao alterar os dados", 400
+
+
+@app.route("/cliente/<int:id>", methods=['DELETE'])
+def delete_cliente(id):
+    try:
+        cliente = postgres.session.query(Clientes).get(id)
+
+        postgres.session.delete(cliente)
+        postgres.session.commit()
+        return "Cliente Excluido com Sucesso", 201
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro ao excluir o cliente", 400
+
+@app.route("/pedidos", methods=["GET"])
+def get_pedidos():
+    try:
+        pedidos = pedidos_collection.find()
+
+        # Convertendo ObjectId em strings para serialização
+        pedidos_serializaveis = []
+        for pedido in pedidos:
+            pedido['_id'] = str(pedido['_id'])
+            pedidos_serializaveis.append(pedido)
+
+        return jsonify(pedidos_serializaveis), 200
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro ao listar pedidos.", 500
+
+# Rotas para Create, Update e Delete de pedidos)
+@app.route("/pedido", methods=["POST"])
+def set_pedido():
+    try:
+        dados = request.get_json()
+        novo_pedido = Pedidos(
+            id_produto=dados["id_produto"],
+            id_cliente=dados['id_cliente'],
+            data_pedido=dados["data_pedido"],
+            valor_pedido=dados["valor_pedido"]
+        )
+        resultado = pedidos_collection.insert_one(novo_pedido.serialize())
+        if resultado.inserted_id:
+            # Retorna o pedido recém-criado e o status 201
+            novo_pedido.id_pedido = str(resultado.inserted_id)
+            return jsonify(novo_pedido.serialize()), 201
+        else:
+            return "Erro ao inserir pedido.", 500
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro ao inserir pedido.", 400
+
+@app.route("/pedido/<pedido_id>", methods=['DELETE'])
+def delete_pedido(pedido_id):
+    try:
+        if not ObjectId.is_valid(pedido_id):
+            return "ID de pedido inválido.", 400
+
+        resultado = pedidos_collection.delete_one({"_id": ObjectId(pedido_id)})
+
+        # Verifica se o pedido foi encontrado e excluído
+        if resultado.deleted_count == 1:
+            return (f"Pedido com ID {pedido_id} excluído com sucesso."), 200
+        else:
+            return (f"Pedido com ID {pedido_id} não encontrado."), 404
+    except Exception as e:
+        return f"Erro ao excluir pedido: {e}", 500
+
+@app.route("/pedido/<pedido_id>", methods=["PUT"])
+def update_pedido(pedido_id):
+    try:
+        if not ObjectId.is_valid(pedido_id):
+            return "ID de pedido inválido.", 400
+
+        # Obtém os novos dados do pedido do corpo da solicitação
+        dados = request.get_json()
+
+        # Atualiza o pedido no banco de dados
+        resultado = pedidos_collection.update_one(
+            {"_id": ObjectId(pedido_id)},
+            {"$set": dados}  # Use $set para atualizar apenas os campos fornecidos
+        )
+
+        # Verifica se o pedido foi encontrado e atualizado
+        if resultado.modified_count == 1:
+            return f"Pedido com ID {pedido_id} atualizado com sucesso.", 200
+        else:
+            return f"Pedido com ID {pedido_id} não encontrado ou nenhum dado foi modificado.", 404
+    except Exception as e:
+        return f"Erro ao atualizar pedido: {e}", 500
+
 
 
 if __name__ == "__main__":
